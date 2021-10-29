586895a3ed62763d8c660eb176029e001caf1619
Lucas Andrade <andradelucas94@gmail.com>
2021-10-29 11:03:00 -0600

    Implemented cartIcon and cart-dropdown component, and cartReducer

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "./sign-up.style.scss";
+import "./sign-up.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
