b7b3877a19bfd06fe3078e222e9208533b481637
Lucas Andrade <andradelucas94@gmail.com>
2021-11-02 14:20:29 -0600

    Implemented shop reducer and selector, and refactored shop component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,28 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import SHOP_DATA from "./shop.data";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => {
-          return <CollectionPreview key={id} {...otherCollectionProps} />;
-        })}
-      </div>
-    );
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
